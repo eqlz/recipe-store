6c43bc6ad38ab498ce3778bae4220723180086f4
Emily Zhang <qinglu.zh@gmail.com>
2019-06-24 23:01:27 -0400

    fill layout in recipe-list component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Array<Recipe> = [
+    new Recipe(
+    'Pasta',
+    'Simple pasta salad',
+    'https://c.pxhere.com/photos/32/af/close_up_cuisine_delicious_diet_dish_epicure_food_healthy-1173367.jpg!d'
+    )
+  ];
   constructor() { }
 
   ngOnInit() {
