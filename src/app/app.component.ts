a1fbb63c4da76586ca5a1a5f088a6d2e783773f2
Emily Zhang <qinglu.zh@gmail.com>
2019-06-25 21:16:53 -0400

    allow nagviation between recipe and shopping list via event emitter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipe-store';
+  showRecipe: boolean = true;
+
+  onShowRecipe(event) {
+    this.showRecipe = event.isRecipeShown;
+  }
+
+  onShowShoppingList(event) {
+    this.showRecipe = event.isRecipeShown;
+  }
 }
