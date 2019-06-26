58e24dc5c55f5ec3c43603d1b3467d526fb82ae5
Emily Zhang <qinglu.zh@gmail.com>
2019-06-25 22:19:51 -0400

    show selected recipe in recipe detail component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
@@ -8,14 +8,25 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   recipes: Array<Recipe> = [
     new Recipe(
-    'Pasta',
-    'Simple pasta salad',
-    'https://c.pxhere.com/photos/32/af/close_up_cuisine_delicious_diet_dish_epicure_food_healthy-1173367.jpg!d'
+      'Pasta',
+      'Simple pasta salad',
+      'https://c.pxhere.com/photos/32/af/close_up_cuisine_delicious_diet_dish_epicure_food_healthy-1173367.jpg!d'
+    ),
+    new Recipe(
+      'Ice coffee',
+      'Quick Ice coffee',
+      'https://www.happyfoodstube.com/wp-content/uploads/2016/07/1-Minute-Instant-Iced-Coffee-Recipe.jpg'
     )
   ];
+  @Output() passRecipeSelected = new EventEmitter<Recipe>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelectedRecipe(recipeConfig: Recipe) {
+    this.passRecipeSelected.emit(recipeConfig);
   }
 
 }
