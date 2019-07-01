22d3d76e4e0b6aaaa96d5356563b8fae16d5d289
Emily Zhang <qinglu.zh@gmail.com>
2019-07-01 10:00:45 -0400

    add recipe service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,28 +1,19 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Array<Recipe> = [
-    new Recipe(
-      'Pasta',
-      'Simple pasta salad',
-      'https://c.pxhere.com/photos/32/af/close_up_cuisine_delicious_diet_dish_epicure_food_healthy-1173367.jpg!d'
-    ),
-    new Recipe(
-      'Ice coffee',
-      'Quick Ice coffee',
-      'https://www.happyfoodstube.com/wp-content/uploads/2016/07/1-Minute-Instant-Iced-Coffee-Recipe.jpg'
-    )
-  ];
+  recipes: Array<Recipe>;
   @Output() passRecipeSelected = new EventEmitter<Recipe>();
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   onSelectedRecipe(recipeConfig: Recipe) {
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,20 @@
+import { Recipe } from './recipe.model';
+
+export class RecipeService {
+  private recipes: Array<Recipe> = [
+    new Recipe(
+      'Pasta',
+      'Simple pasta salad',
+      'https://c.pxhere.com/photos/32/af/close_up_cuisine_delicious_diet_dish_epicure_food_healthy-1173367.jpg!d'
+    ),
+    new Recipe(
+      'Ice coffee',
+      'Quick Ice coffee',
+      'https://www.happyfoodstube.com/wp-content/uploads/2016/07/1-Minute-Instant-Iced-Coffee-Recipe.jpg'
+    )
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+}
