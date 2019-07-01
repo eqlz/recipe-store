01c6eb741e6dffa4700bcb62a3ce3e224b4fe452
Emily Zhang <qinglu.zh@gmail.com>
2019-07-01 12:41:35 -0400

    remove default ingredient

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -3,10 +3,7 @@ import { EventEmitter } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 
 export class ShoppingListService {
-  ingredients: Array<Ingredient> = [
-    // new Ingredient('pasta', 10),
-    // new Ingredient('veggie', 8),
-  ];
+  ingredients: Array<Ingredient> = [];
 
   ingredientAdded = new EventEmitter<Array<Ingredient>>();
 
