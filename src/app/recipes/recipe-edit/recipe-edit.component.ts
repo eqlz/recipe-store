d5d5edea4838aa89cbeaed033bc662c4651d3bb7
Emily Zhang <qinglu.zh@gmail.com>
2019-07-21 18:01:16 -0400

    show ingedients in a recipe via form array

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
 import { RecipeService } from '../recipe.service';
 
 @Component({
@@ -33,22 +33,36 @@ export class RecipeEditComponent implements OnInit {
     console.log(this.recipeForm);
   }
 
+  get controls() {
+    return (this.recipeForm.get('ingredients') as FormArray).controls;
+  }
+
   private initForm() {
     let recipeName = '';
     let recipeImagePath = '';
     let recipeDescription = '';
+    let recipeIngredients = new FormArray([]);
 
     if (this.editMode) {
       const recipeToEdit = this.recipeService.getRecipeByIndex(this.id);
       recipeName = recipeToEdit.name;
       recipeImagePath = recipeToEdit.imagePath;
       recipeDescription = recipeToEdit.description;
+      if (recipeToEdit.ingredients) {
+        for (const ingredient of recipeToEdit.ingredients) {
+          recipeIngredients.push(new FormGroup({
+            name: new FormControl(ingredient.name),
+            amount: new FormControl(ingredient.amount)
+          }));
+        }
+      }
     }
 
     this.recipeForm = new FormGroup({
       name: new FormControl(recipeName),
       imagePath: new FormControl(recipeImagePath),
-      description: new FormControl(recipeDescription)
+      description: new FormControl(recipeDescription),
+      ingredients: recipeIngredients
     });
   }
 
