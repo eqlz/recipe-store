27457b54bf4dd1d1acba69f8f97246a33502630f
Emily Zhang <qinglu.zh@gmail.com>
2019-07-21 22:06:07 -0400

    add ingredients key in fetched data if no ingredients in response

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -23,6 +24,13 @@ export class DataStorageService {
   fetchRecipe() {
     this.http.get<Array<Recipe>>(
       'https://recipe-store-79749.firebaseio.com/recipes.json'
+    )
+    .pipe(
+      map(recipes => {
+        return recipes.map(recipe => {
+          return { ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] };
+        });
+      })
     )
     .subscribe(recipes => {
       this.recipeService.setRecipes(recipes);
